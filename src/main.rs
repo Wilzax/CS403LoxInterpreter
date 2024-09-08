78446505009d070915db01fcd30ce4102ece7cf0
wking <wilsonking0256@gmail.com>
2024-09-07 23:16:59 -0500

    Updated comments to show citations more clearly

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,32 @@
-/**********
- * Starts the interpreter's execution by managing the arguments passed at runtime
- * Possible aruments:
- *  none: start in interactive mode using stdin
- *  1 or more: use first argument as filepath for code to interpret, ignore other arguments
- **********/
+//! Starts the interpreter's execution by managing the arguments passed at runtime
+//! Possible aruments:
+//!   0 - start in interactive mode using stdin
+//!   1 - use first argument as filepath for code to interpret
+//!   2 or more - undefined, should use 1st argument only and ignore the rest
 
 use std::env;
-use std::fs::File;
-
 mod scanner;
 
 fn main() {
-    println!("Lox interpreter in Rust");
-    println!("Written by Wilson King, Alicia Reed, Kiera Schnell, and Brodye Stevens\n");
-
     
+    //Print version and authors before executing
+    println!("Lox interpreter v0.0.2:pre-scanning in Rust");
+    println!("Written by Wilson King, Alicia Reed, Kiera Schnell, and Brodye Stevens\n");
+    
+    //Decide on interactive or file read mode
     let args: Vec<String> = env::args().collect();
     //println!("Detected {} main arguments", args.len());
     if args.len() < 2 {
         println!("No file supplied, starting in interactive mode...");
         //TODO: Call to scanner using stdio
         //dbg!(args);
-        panic!("No scanner implemented for interactive mode") //Delete when scanner is implemented
+        println!("No scanner implemented for interactive mode"); //Delete when scanner is implemented
     } 
     else {
-        let input_file_result:Result<File, std::io::Error> = File::open(args[1].clone());
         println!("Attempting to open file {}...", args[1]);
-        
-        /* From https://doc.rust-lang.org/book/ch09-02-recoverable-errors-with-result.html */
-        let _input_file = match input_file_result { 
-            Ok(file) => file,
-            Err(error) => panic!("Problem opening the file: {error:?}"),
-        };
-        /* End citation */
 
         //TODO: Call to scanner using input_file (remove leading underscore when implemented)
-        panic!("No scanner implemented for file read mode") //Delete when scanner is implemented
+        println!("No scanner implemented for file read mode") //Delete when scanner is implemented
     }
+    std::process::exit(1)
 }
--- src/scanner.rs
+++ src/scanner.rs
@@ -1,14 +1,29 @@
-use std::collections::HashMap;
+//! The purpose of a scanner is to take an input stream and tokenize it.
+//! The scanner: 
+//!   defines the enum for list of possible tokens,
+//!   opens an iostream,
+//!     if interactive, use stdio       ##TODO: find actual package name
+//!     if file supplied, use std::fs::File
+//!   breaks the [stream? buffer?] into Strings,
+//!   breaks Strings into tokens,
+//!   matches tokens from stream to tokens in enum
+//!     in the case of literals or identifiers, passes the value as well
+//!   pushes matched tokens into vector
+//!   calls the parser on the vector of token enums
 
-#[derive(Eq, PartialEq, Debug, Copy, Clone)]
-pub enum TokenType {
+mod scanner{
+    use std::fs::File;
+    use std::io::{self, BufRead};
+    use std::path::Path;
+
+    /// From https://craftinginterpreters.com/scanning.html
+    /// All possible tokens in Lox language, reformatted from Java enum to Rust enum
+    enum TokenType {
     // Single-character tokens.
     LeftParen,
     RightParen,
     LeftBrace,
     RightBrace,
-    LeftBracket,
-    RightBracket,
     Comma,
     Dot,
     Minus,
@@ -16,7 +31,7 @@ pub enum TokenType {
     Semicolon,
     Slash,
     Star,
-
+    
     // One or two character tokens.
     Bang,
     BangEqual,
@@ -26,15 +41,15 @@ pub enum TokenType {
     GreaterEqual,
     Less,
     LessEqual,
-
+    
     // Literals.
-    Identifier,
-    String,
-    Number,
-
+    Identifier(String),
+    String(String),
+    Number(f64),
+    
     // Keywords.
     And,
-    Class,
+    Delete,
     Else,
     False,
     Fun,
@@ -49,7 +64,20 @@ pub enum TokenType {
     True,
     Var,
     While,
-    Lambda,
-
+    
     Eof,
+    }
+    // End citation for enum TokenType
+
+    fn read_line_stdin() {
+        
+    }
+    /// From https://doc.rust-lang.org/stable/rust-by-example/std_misc/file/read_lines.html
+    /// Opens a file from a given filename, and returns an iterable type for each line of the file
+    fn read_file_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where P: AsRef<Path>, {
+        let file = File::open(filename)?;
+        Ok(io::BufReader::new(file).lines())
+    }
+    // End citation for fn read_file_lines
 }
