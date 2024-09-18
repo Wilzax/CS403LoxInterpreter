4b41c834e81963558199553e01bfb70fd096cd91
“kieraschnell <kieraschnell@comcast.net>
2024-09-18 11:53:52 -0500

    making the file

---
+++ src/expr.rs
@@ -0,0 +1,21 @@
+use crate::token::Token;
+
+#[derive(Debug)]
+pub enum Expr {
+    Binary {
+        left: Box<Expr>,
+        operator: Token,
+        right: Box<Expr>,
+    },
+    Grouping {
+        expression: Box<Expr>,
+    },
+    Literal {
+        value: Literal,
+    },
+    Unary {
+        operator: Unary,
+        right: Box<Expr>,
+    },
+}
+
