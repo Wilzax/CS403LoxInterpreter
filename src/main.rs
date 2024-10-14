20c81ab733577cf07fc50ac1a636ac3264d6d745
“kieraschnell <kieraschnell@comcast.net>
2024-10-13 20:59:56 -0500

    file input reading

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,12 @@ mod stmt;
 mod environment;
 use std::env::args;
 use std::fs::File;
+use std::io::Read;
 
 fn main() {
     println!("");
     scanner::run("var a = 0;\n var temp;\n var count = 0;\n for (var b = 1; a < 10000; b = temp + b) {{\n print a;\n temp = a;\n a = b;\ncount = count + 1;\n}}\n print count;".to_string());
-
+    
     let args: Vec<String> = args().collect();
     println!("Detected {} main arguments", args.len());
     if args.len() < 2 {
@@ -18,15 +19,24 @@ fn main() {
         dbg!(args);
     } 
     else {
-        let input_file_result = File::open(args[1].clone());
-        println!("Attempting to open file {}...", args[1]);
-        
-        //From https://doc.rust-lang.org/book/ch09-02-recoverable-errors-with-result.html
-        let _input_file = match input_file_result { 
+        let file_path = &args[1];
+        println!("Attempting to open file {}...", file_path);
+
+        let input_file_result = File::open(file_path);
+
+        let mut input_file = match input_file_result { 
             Ok(file) => file,
             Err(error) => panic!("Problem opening the file: {error:?}"),
         };
-        //End citation
+
+        let mut file_contents = String::new();
+        match input_file.read_to_string(&mut file_contents) {
+            Ok(_) => {
+                scanner::run(file_contents);
+            },
+            Err(error) => panic!("Problem reading the file: {error:?}"),
+        };
     }
-    println!("Thus ends the program.")
+
+    println!("Thus ends the program.");
 }
